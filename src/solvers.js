4835621270020f867ea1d507d662aab8e36ed532
MKS-Student <mks.student@hackreactor.com>
2015-03-12 11:33:22 -0500

    rooks and queens work up to a point, but then we get stack overflow

--- src/solvers.js
+++ src/solvers.js
@@ -14,7 +14,52 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n:n});
+  var firstRow = 0;
+  var firstCol = 0;
+  var solution = [];
+  var colTracker = Array(n);
+  var testSpot = function(row, col){
+    // if the input is invalid, something's gone wrong
+    if (col < 0 || row < 0){
+      return;
+    }
+    // if the whole first row is empty, game over
+    if (col >= n && row === 0){
+      return;
+    }
+    // if the col is off board, ask parentCol for place to recurse
+    if (col >= n) {
+      var parentCol = colTracker[row - 1];
+      // delete the previous row's spot
+      board.get(row - 1)[parentCol] = 0;
+      testSpot(row - 1, (parentCol + 1));
+    }
+    // set the next spot to 1 in prep for testing
+    board.get(row)[col] = 1;
+    // test the spot for conflicts
+    if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()){
+      // conflict found
+      board.get(row)[col] = 0;
+      testSpot(row, col + 1);
+    } else {
+      // no conflict found
+      // leave it there
+      // update colTracker
+      colTracker[row] = col;
+      // check if next is off board
+      if (row === (n - 1)){
+        for (var i = 0; i < n; i++) {
+          solution.push(board.get(i));
+        };
+        //return solution;
+      } else {
+        // call test Spot on next row
+        testSpot(row + 1, 0);
+      }
+    }
+  }
+  testSpot(firstRow, firstCol);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,7 +69,58 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
+  var firstRow = 0;
+  var firstCol = 0;
+  //var solution = [];
+  var colTracker = Array(n);
+  var testSpot = function(row, col){
+    // if the input is invalid, something's gone wrong
+    if (col < 0 || row < 0){
+      return;
+    }
+    // if the whole first row is empty, game over
+    if (col >= n && row === 0){
+      return;
+    }
+    // if the col is off board, ask parentCol for place to recurse
+    if (col >= n) {
+      var parentCol = colTracker[row - 1];
+      // delete the previous row's spot
+      board.get(row - 1)[parentCol] = 0;
+      return testSpot((row - 1), (parentCol + 1));
+    }
+    // set the next spot to 1 in prep for testing
+    board.get(row)[col] = 1;
+    // test the spot for conflicts
+    if (board.hasAnyRowConflicts() || board.hasAnyColConflicts()){
+      // conflict found
+      board.get(row)[col] = 0;
+      testSpot(row, col + 1);
+    } else {
+      // no conflict found
+      // leave it there
+      // update colTracker
+      colTracker[row] = col;
+      // check if next is off board
+      if (row === (n - 1)){
+        //if solution, keep going
+        solutionCount++;
+
+        board.get(row)[col] = 0;
+        testSpot(row, col + 1);
+
+      } else {
+        // call test Spot on next row
+        testSpot(row + 1, 0);
+      }
+    }
+  }
+  testSpot(firstRow, firstCol);
+
+
+
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -34,7 +130,53 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n:n});
+  var firstRow = 0;
+  var firstCol = 0;
+  var solution = [];
+  var colTracker = Array(n);
+  var testSpot = function(row, col){
+    // if the input is invalid, something's gone wrong
+    if (col <=0 || row <=0){
+      return;
+    }
+    // if the whole first row is empty, game over
+    if (col >= n && row === 0){
+      return;
+    }
+    // if the col is off board, ask parentCol for place to recurse
+    if (col >= n) {
+      var parentCol = colTracker[row - 1];
+      // delete the previous row's spot
+      board.get(row - 1)[parentCol] = 0;
+      testSpot(row - 1, (parentCol + 1));
+    }
+    // set the next spot to 1 in prep for testing
+    board.get(row)[col] = 1;
+    // test the spot for conflicts
+    if (board.hasAnyQueensConflicts()){
+      // conflict found
+      board.get(row)[col] = 0;
+      testSpot(row, col + 1);
+    } else {
+      // no conflict found
+      // leave it there
+      // update colTracker
+      colTracker[row] = col;
+      // check if next is off board
+      if (row === (n - 1)){
+        for (var i = 0; i < n; i++) {
+          solution.push(board.get(i));
+        };
+        //return solution;
+      } else {
+        // call test Spot on next row
+        testSpot(row + 1, 0);
+      }
+    }
+  }
+  testSpot(firstRow, firstCol);
+  
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -43,7 +185,55 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
+  var firstRow = 0;
+  var firstCol = 0;
+  //var solution = [];
+  var colTracker = Array(n);
+  var testSpot = function(row, col){
+    // if the input is invalid, something's gone wrong
+    if (col < 0 || row < 0){
+      return;
+    }
+    // if the whole first row is empty, game over
+    if (col >= n && row === 0){
+      return;
+    }
+    // if the col is off board, ask parentCol for place to recurse
+    if (col >= n) {
+      var parentCol = colTracker[row - 1];
+      // delete the previous row's spot
+      board.get(row - 1)[parentCol] = 0;
+      return testSpot((row - 1), (parentCol + 1));
+    }
+    // set the next spot to 1 in prep for testing
+    board.get(row)[col] = 1;
+    // test the spot for conflicts
+    if (board.hasAnyQueensConflicts()){
+      // conflict found
+      board.get(row)[col] = 0;
+      testSpot(row, col + 1);
+    } else {
+      // no conflict found
+      // leave it there
+      // update colTracker
+      colTracker[row] = col;
+      // check if next is off board
+      if (row === (n - 1)){
+        //if solution, keep going
+        solutionCount++;
+
+        board.get(row)[col] = 0;
+        testSpot(row, col + 1);
+
+      } else {
+        // call test Spot on next row
+        testSpot(row + 1, 0);
+      }
+    }
+  }
+  testSpot(firstRow, firstCol);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
